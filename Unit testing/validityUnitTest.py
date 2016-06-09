fde6b212c0889fcf84ae4ad6ac6c2887466f7e7f
Dipto Das <DiptoDas8@users.noreply.github.com>
2016-06-10 01:01:03 +0600

    Update validityUnitTest.py

--- Unit testing/validityUnitTest.py
+++ Unit testing/validityUnitTest.py
@@ -5,13 +5,9 @@ class validityTestCase(unittest.TestCase):
 	def testIsGivenValidInput(self):
 	#max_price, min_price
 		self.assertTrue(isValid(67,7))	#max>min	--valid
-<<<<<<< HEAD
 	def testIsGivenValidInput2(self):
 		self.assertFalse(isValid(5,7))
 	def testIsGivenValidInput3(self):
-=======
-		self.assertFalse(isValid(5,7))
->>>>>>> 95d0d2b309f190152f39abf27546bc5ffdfe3b22
 		self.assertFalse(isValid("a",7))
 
 if __name__ == '__main__':
